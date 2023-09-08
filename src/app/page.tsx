66a65b5b6ee7ac42c114daf0593d3ca6e63add3a
IgorAlvaredo <94122674+IgorAlvaredo@users.noreply.github.com>
2023-09-08 17:04:05 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,35 @@
-import Image from 'next/image'
-import Layout from '../components/Layout'
-import Tabela from '@/components/Tabela'
-import Cliente from '@/core/Cliente'
+'use client';
+import Image from "next/image";
+import Layout from "../components/Layout";
+import Tabela from "@/components/Tabela";
+import Cliente from "@/core/Cliente";
 
 export default function Home() {
-
   const clientes = [
-    new Cliente('Ana', 34, '1'),
-    new Cliente('Bia', 45, '2'),
-    new Cliente('Pedro', 23, '3'),
-    new Cliente('Carlos', 54, '4')
-  ]
+    new Cliente("Ana", 34, "1"),
+    new Cliente("Bia", 45, "2"),
+    new Cliente("Pedro", 23, "3"),
+    new Cliente("Carlos", 54, "4"),
+  ];
+
+  function clienteSelecionado(cliente: Cliente) {}
+
+  function clienteExcluido(cliente: Cliente) {}
 
   return (
-    <div className={`
+    <div
+      className={`
      flex justify-center items-center h-screen
      bg-gradient-to-r from-blue-500 to-purple-500
-     text-white`}>
+     text-white`}
+    >
       <Layout titulo="Cadastro Simples">
-        <Tabela clientes={clientes}></Tabela>
+        <Tabela
+          clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        />
       </Layout>
     </div>
-  )
+  );
 }
--- src/components/Tabela.tsx
+++ src/components/Tabela.tsx
@@ -1,45 +1,88 @@
-import Cliente from "@/core/Cliente"
+'use client';
+import Cliente from "@/core/Cliente";
+import { IconeEdicao, IconeLixo } from "./icones";
 
 interface TabelaProps {
-    clientes: Cliente[]
+  clientes: Cliente[];
+  clienteSelecionado?: (client: Cliente) => void;
+  clienteExcluido?: (client: Cliente) => void;
 }
 
 export default function Tabela(props: TabelaProps) {
+  const exibiAcoes = props.clienteSelecionado || props.clienteExcluido;
 
-    function renderizarCabecalho() {
-        return (
-            <tr>
-                <th className="text-left p-4">Código</th>
-                <th className="text-left p-4">Nome</th>
-                <th className="text-left p-4">Idade</th>
-            </tr>
-        )
-    }
-
-    function renderizarDados() {
-        return props.clientes.map((cliente, i) => {
-            return (
-                <tr key={cliente.id}
-                    className={`${i%2==0?'bg-purple-200':'bg-purple-100'}`}>
-                    <td className="text-left p-4">{cliente.id}</td>
-                    <td className="text-left p-4">{cliente.nome}</td>
-                    <td className="text-left p-4">{cliente.idade}</td>
-                </tr>
-            )
-        })
-    }
-
+  function renderizarCabecalho() {
     return (
-        <table className="w-full rounded-xl overflow-hidden ">
-            <thead className={`
+      <tr>
+        <th className="text-left p-4">Código</th>
+        <th className="text-left p-4">Nome</th>
+        <th className="text-left p-4">Idade</th>
+        {exibiAcoes ? <th className=" p-4">Ações</th> : false}
+      </tr>
+    );
+  }
+
+  function renderizarDados() {
+    return props.clientes.map((cliente, i) => {
+      return (
+        <tr
+          key={cliente.id}
+          className={`${i % 2 == 0 ? "bg-purple-200" : "bg-purple-100"}`}
+        >
+          <td className="text-left p-4">{cliente.id}</td>
+          <td className="text-left p-4">{cliente.nome}</td>
+          <td className="text-left p-4">{cliente.idade}</td>
+          {exibiAcoes ? renderizarAcoes(cliente) : false}
+        </tr>
+      );
+    });
+  }
+
+  function renderizarAcoes(cliente: Cliente) {
+    return (
+      <td className="flex justify-center">
+        {props.clienteSelecionado ? (
+          <button onClick={() => props.clienteSelecionado?.(cliente)}
+            className={`
+         flex justify-center items-center
+         text-green-600 rounded-full hover:bg-purple-50
+          p-2 m-1
+        `}
+          >
+            {IconeEdicao}
+          </button>
+        ) : (
+          false
+        )}
+
+        {props.clienteExcluido ? (
+          <button onClick={() => props.clienteExcluido?.(cliente)}
+            className={`
+         flex justify-center items-center
+         text-red-500 rounded-full hover:bg-purple-50
+          p-2 m-1
+        `}
+          >
+            {IconeLixo}
+          </button>
+        ) : (
+          false
+        )}
+      </td>
+    );
+  }
+
+  return (
+    <table className="w-full rounded-xl overflow-hidden ">
+      <thead
+        className={`
              text-gray-100
              bg-gradient-to-r from-purple-600 to-purple-800
-            `}>
-                {renderizarCabecalho()}
-            </thead>
-            <tbody>
-                {renderizarDados()}
-            </tbody>
-        </table>
-    )
+            `}
+      >
+        {renderizarCabecalho()}
+      </thead>
+      <tbody>{renderizarDados()}</tbody>
+    </table>
+  );
 }
